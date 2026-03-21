078f4557383786f0305339775cf948b324f6cafd
Hrajix <hrajix2@gmail.com>
2026-03-21 10:25:20 +0100

    fix: update import path for router in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router"; // TADY nesmí být "-dom"
-import { router } from "./routes"; // nebo cesta k tvému routes.tsx
+import { router } from "./app/routes"; // nebo cesta k tvému routes.tsx
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
